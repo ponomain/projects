d76cef21f05c6ca070c6edf184cf860bddbb7fd0
Nikolai Ponomarev <isaveryan@mail.ru>
2022-05-13 17:06:12 +0400

    adding card issuer

---
+++ CardIssuer/client/src/test/java/ponomarev/nickolay/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ponomarev.nickolay.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
